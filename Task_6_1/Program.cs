d8cc17e58a3429cf9c163d96168865e635a5f799
metam0rf0zer <sdpsdp88@gmail.com>
2025-03-28 14:19:29 +0300

    ДЗ6

---
+++ Task_6_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel.DataAnnotations;
+Console.WriteLine("Напишите предложение");
+string text = Console.ReadLine();
+string[] atext = text.Split(' ');
+int length=atext.Length;
+string word = atext[0];  
+for (int i = 0; i < length; i++)
+{
+    if (word.Length < atext[i].Length)
+    {
+        word = atext[i];
+    }
+    
+}
+Console.WriteLine("Самое длинное слово в предложении - {0}",word);
