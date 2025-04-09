c99707ce29212e77780fc6efd668f497e63a686f
metam0rf0zer <sdpsdp88@gmail.com>
2025-04-09 11:28:56 +0300

    ДЗ8

--- test12/Program.cs
+++ test12/Program.cs
@@ -1,41 +1,43 @@
-﻿const int n = 3;
-int[,] array = { { 0, 0, 0 }, { 1, 1, 1 }, { 0, 0, 0 } };
+﻿using System;
 
-for (int i = 0; i < n; i++)
+namespace Task_8_2
 {
-    for (int j = 0; j < n; j++)
+    internal class Program
     {
-        Console.Write("{0} ", array[i, j]);
-    }
-    Console.WriteLine();
-}
-
-
-
-
-//Проверка по горизонтали
-bool flag = false;
-int cnt = 0;
-for (int i = 0; i < n; i++)
-{
-    cnt = 0;
-    for (int j = 0; j < n; j++)
-    {
-        if (j < 2 && array[i, 0] == 1)
+        static void Main(string[] args)
         {
-            cnt = array[i, j] == array[i, j + 1] && array[i, j] == 1 ? cnt + 1 : cnt;
+            try
+            {
+                ValidateAge(Convert.ToInt32(Console.ReadLine()));
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
         }
 
+        static void ValidateAge(int age)
+        {
+            if (age < 0)
+                throw new ArgumentException(paramName: null, message:$"Возраст не может быть отрицательным");
 
+            if (age > 150)
+                throw new ArgumentOutOfRangeException(paramName: null, message: $"Возраст слишком большой");
+
+        }
     }
-
-    flag = cnt == 2 ? true : false;
-    if (flag == true)
-    {
-        break;
-    }
-
 }
-Console.WriteLine(flag);
-
-
