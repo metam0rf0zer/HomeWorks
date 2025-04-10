a77eb51fc7b41a4fc3019776a5cafbcd01925391
metam0rf0zer <sdpsdp88@gmail.com>
2025-04-10 12:49:11 +0300

    ДЗ9

---
+++ Task_9_1/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace Task_9_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();      
+            
+            book.Title = "Война и мир";
+            book.Author = "Л.Толстой";
+
+            book.Getinfo();
+
+        }
+
+
+
+
+
+    }
+    class Book
+    {
+        string _title;
+        string _author;
+        int Year;
+        int Pages;
+
+        public string Title
+        {
+            set
+            {
+                this._title = value;
+            }
+            get
+            {
+                return _title;
+            }        
+        }
+
+        public string Author
+        {
+            set
+            {
+                this._author = value;
+            }
+            get
+            {
+                return _author;
+            }
+        }
+
+
+
+        public void Getinfo()
+        {
+            Console.WriteLine($"{Title},{_author},{Year},{Pages} стр.");
+        }
+
+            
+        
+
+       
+
+
+
+
+
+    }
+
+
+}
