d5e0784beab44d3b3b7dbdc6ad45c81f94ad5c1e
metam0rf0zer <sdpsdp88@gmail.com>
2025-05-06 16:47:39 +0300

    1

---
+++ Task_11_1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Task_11_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal[] animals = new Animal[]
+            {
+                new Dog(),                               
+                new Cat(),
+            };
+
+
+            foreach (Animal animal in animals)
+            {
+                Console.WriteLine(animal.ShowInfo());
+            }
+        }
+    }
+    public abstract class Animal
+    {
+        public abstract string Name { get; }
+        public abstract string Say();
+        public string ShowInfo()
+        {
+            return $"Животное {Name} говорит {Say()}";
+        }
+    }
+    public class Dog : Animal
+    {
+
+        public override string Name => "Собака";
+        public override string Say()
+        {
+            return $"Гав!";
+        }
+    }
+    public class Cat : Animal
+    {
+
+        public override string Name => "Кошка";
+        public override string Say()
+        {
+            return $"Мяу!";
+        }
+    }
+
+}
