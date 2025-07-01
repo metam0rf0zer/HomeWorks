7ad964a1cfe2d85c058ca63dcec86493103426c7
metam0rf0zer <sdpsdp88@gmail.com>
2025-07-01 15:13:35 +0300

    WPF ДЗ1

--- WPF_Task_1_3/MainWindow.xaml.cs
+++ WPF_Task_1_3/MainWindow.xaml.cs
@@ -16,9 +16,29 @@ namespace WPF_Task_1_3
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random randomPosition = new Random();
+        
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Grid_MouseEnter(object sender, MouseEventArgs e)
+        {
+            double horizontal = randomPosition.Next(0, 650);
+            double vertical = randomPosition.Next(0, 400);
+            myButton.Margin=new Thickness(horizontal,vertical,0,0);
+        }
+
+        private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            myButton.Content = "Молодец, поймал!";
+        }
+
+        private void myButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            myButton.Content = "Поймай меня!";
         }
     }
 }
