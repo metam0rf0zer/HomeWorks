eebe3872e12370b249d451ea3ef3fb3132497009
metam0rf0zer <sdpsdp88@gmail.com>
2025-07-10 17:14:45 +0300

    Доработка HW3

---
+++ WPF_Task_3_1/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Task_3_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Apply.IsChecked.HasValue|| !Apply.IsChecked.Value)
+            {
+                ErrorWindow errorWindow = new ErrorWindow();
+                errorWindow.WindowStyle=WindowStyle.ToolWindow;
+                errorWindow.ShowDialog();
+            }
+        }
+
+        private void Apply_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
