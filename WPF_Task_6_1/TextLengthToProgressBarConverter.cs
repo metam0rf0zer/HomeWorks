a2bf8ee08644c7c7c96ac552538e8dc62e839f01
Sterling Archer <sdpsdp88@gmail.com>
2025-08-10 23:56:22 +0300

    ДЗ6

---
+++ WPF_Task_6_1/TextLengthToProgressBarConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WPF_Task_6_1
+{
+    class TextLengthToProgressBarConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string text)
+            {
+                return text.Length;
+            }
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
