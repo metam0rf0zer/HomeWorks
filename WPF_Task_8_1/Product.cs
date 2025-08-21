398e9bf874bf3a4d9d303b532a5e396ec54fb2d2
Sterling Archer <sdpsdp88@gmail.com>
2025-08-21 22:57:37 +0300

    ДЗ8 из дома

---
+++ WPF_Task_8_1/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Task_8_1
+{
+    public enum ProductTypes
+    {
+        Food,
+        HomeAppliances
+    }
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public double Price {  get; set; }
+        public string Image {  get; set; }
+        public ProductTypes ProductType { get; set; }
+    }
+}
