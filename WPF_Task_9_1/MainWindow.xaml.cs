a094fe328cc66fe9d0be6ecb81185920f77035b3
Sterling Archer <sdpsdp88@gmail.com>
2025-08-26 16:15:02 +0300

    1

---
+++ WPF_Task_9_1/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Task_9_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CurrentColor = Brushes.White;
+        }
+
+        private void ChangeColor(object sender, ExecutedRoutedEventArgs e)
+        {
+            var random = new Random();
+            var color = Color.FromRgb(
+                (byte)random.Next(256),
+                (byte)random.Next(256),
+                (byte)random.Next(256)
+            );
+            CurrentColor = new SolidColorBrush(color);
+        }
+    }
+}
