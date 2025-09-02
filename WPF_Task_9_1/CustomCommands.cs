497d414dd27e4cfa7b42e2ce641c2cb48f088521
Sterling Archer <sdpsdp88@gmail.com>
2025-09-02 17:17:58 +0300

    ДЗ9 из дома

--- WPF_Task_9_1/CustomCommands.cs
+++ WPF_Task_9_1/CustomCommands.cs
@@ -8,15 +8,15 @@ using System.Windows.Media;
 
 namespace WPF_Task_9_1
 {
-    internal class CustomCommands
+    public class CustomCommands
     {
-        public static RoutedUICommand ChangeColor {  get; set; }
+        public static RoutedUICommand ChangeColor {  get; private set; }
 
         static CustomCommands()
         {
             InputGestureCollection inputGestureCollection = new InputGestureCollection();
             inputGestureCollection.Add(new KeyGesture(Key.C, ModifierKeys.Control));
-            ChangeColor = new RoutedUICommand("ChangeColor","Изменить цвет",typeof(CustomCommands),inputGestureCollection);
+            ChangeColor = new RoutedUICommand("Изменить цвет", "ChangeColor",typeof(CustomCommands),inputGestureCollection);
             
         }
     }
--- WPF_Task_9_1/MainWindow.xaml.cs
+++ WPF_Task_9_1/MainWindow.xaml.cs
@@ -11,18 +11,36 @@ using System.Windows.Shapes;
 
 namespace WPF_Task_9_1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private Stack<Brush> _colorHistory = new Stack<Brush>();
+        private Brush _currentColor;
+
+        public Brush CurrentColor
+        {
+            get => _currentColor;
+            set
+            {
+                _currentColor = value;               
+                if (_colorHistory.Count == 0 || _colorHistory.Peek() != value)
+                {
+                    _colorHistory.Push(value);
+                }
+                
+                this.Background = _currentColor;
+                
+                CommandManager.InvalidateRequerySuggested();
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            
             CurrentColor = Brushes.White;
         }
 
-        private void ChangeColor(object sender, ExecutedRoutedEventArgs e)
+        private void ChangeColorExecuted(object sender, ExecutedRoutedEventArgs e)
         {
             var random = new Random();
             var color = Color.FromRgb(
@@ -31,6 +49,26 @@ namespace WPF_Task_9_1
                 (byte)random.Next(256)
             );
             CurrentColor = new SolidColorBrush(color);
+        }
+
+        private void ChangeColorCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void UndoExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (_colorHistory.Count > 1)
+            {
+                _colorHistory.Pop();         
+                CurrentColor = _colorHistory.Peek(); 
+            }
+        }
+
+        
+        private void UndoCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = _colorHistory.Count > 1;
         }
     }
 }
