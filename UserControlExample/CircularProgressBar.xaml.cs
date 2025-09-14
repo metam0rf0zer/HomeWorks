1755fd7f1348fe115a47edc46e7c5f8b8c677eda
Sterling Archer <sdpsdp88@gmail.com>
2025-09-14 13:18:05 +0300

    ДЗ из дома

---
+++ UserControlExample/CircularProgressBar.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace UserControlExample
+{
+    /// <summary>
+    /// Логика взаимодействия для CircularProgressBar.xaml
+    /// </summary> 
+    public partial class CircularProgressBar : UserControl
+    {
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register(
+                nameof(Value),
+                typeof(double),
+                typeof(CircularProgressBar),
+                new PropertyMetadata(0.0, OnValueChanged));
+
+        public static readonly DependencyProperty MaximumProperty =
+            DependencyProperty.Register(
+                nameof(Maximum),
+                typeof(double),
+                typeof(CircularProgressBar),
+                new PropertyMetadata(100.0, OnValueChanged));
+
+        // Read-only Dependency Properties
+        private static readonly DependencyPropertyKey ProgressAnglePropertyKey =
+            DependencyProperty.RegisterReadOnly(
+                nameof(ProgressAngle),
+                typeof(double),
+                typeof(CircularProgressBar),
+                new PropertyMetadata(0.0));
+
+        public static readonly DependencyProperty ProgressAngleProperty = ProgressAnglePropertyKey.DependencyProperty;
+
+        private static readonly DependencyPropertyKey PercentageTextPropertyKey =
+            DependencyProperty.RegisterReadOnly(
+                nameof(PercentageText),
+                typeof(string),
+                typeof(CircularProgressBar),
+                new PropertyMetadata("0%"));
+
+        public static readonly DependencyProperty PercentageTextProperty = PercentageTextPropertyKey.DependencyProperty;
+
+        public double Value
+        {
+            get => (double)GetValue(ValueProperty);
+            set => SetValue(ValueProperty, value);
+        }
+
+        public double Maximum
+        {
+            get => (double)GetValue(MaximumProperty);
+            set => SetValue(MaximumProperty, value);
+        }
+
+        public double ProgressAngle
+        {
+            get => (double)GetValue(ProgressAngleProperty);
+            private set => SetValue(ProgressAnglePropertyKey, value);
+        }
+
+        public string PercentageText
+        {
+            get => (string)GetValue(PercentageTextProperty);
+            private set => SetValue(PercentageTextPropertyKey, value);
+        }
+
+        public CircularProgressBar()
+        {
+            InitializeComponent();
+            UpdateProgress();
+        }
+
+        private static void OnValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var progressBar = (CircularProgressBar)d;
+            progressBar.UpdateProgress();
+        }
+
+        private void UpdateProgress()
+        {
+            // Ограничиваем значения
+            if (Value < 0) Value = 0;
+            if (Value > Maximum) Value = Maximum;
+            if (Maximum <= 0) Maximum = 100;
+
+            // Вычисляем прогресс
+            double percentage = Maximum == 0 ? 0 : (Value / Maximum);
+            ProgressAngle = 360 * percentage;
+            PercentageText = $"{(percentage * 100):F0}%";
+        }
+    }
+}
